68c85edf6dda17aaa730ad855917b2299e7d645b
Kherilirli <januarkherilirli@gmail.com>
2023-12-09 09:06:23 +0700

    tugas11

---
+++ pekan11/modul2.py
@@ -0,0 +1,42 @@
+import math
+
+
+def tambah(bil1, bil2):
+    hasil = bil1+bil2
+    print("Hasil Penjumlahan:", hasil)
+
+def kurang(bil1, bil2):
+    hasil = bil1 - bil2
+    print("Hasil Pengurangan:", hasil)
+
+def kali(bil1, bil2):
+    hasil = bil1 * bil2
+    print("Hasil Perkalian:", hasil)
+
+def bagi(bil1, bil2):
+    hasil = bil1 / bil2
+    print("Hasil Pembagian:", hasil)
+
+def pangkat(bil1, bil2):
+    hasil = bil1 ^ bil2
+    print("Hasil Perpangkatan:", hasil)
+
+def modul(bil1, bil2):
+    hasil = bil1 % bil2
+    print("Hasil Modulus:", hasil)
+
+def pembagian_bilbul(bil1, bil2):
+    hasil = bil1 // bil2
+    print("Hasil Pembagian bilangan bulat:", hasil)
+
+def gabungan_operasi(bil1, bil2, bil3):
+    hasil = (bil1 + bil2) * bil3
+    print("Hasil Gabungan Operasi:", hasil)
+
+def operasi_campuran(bil1, bil2, bil3, bil4):
+    hasil = (bil1 - bil2) * bil3 + bil4
+    print("Hasil Operasi Campuran:", hasil)
+
+def akar_kuadrat(bil1):
+    hasil = bil1 * 0.5
+    print("Hasil Akar Kuadrat:", hasil)
---
+++ pekan11/panggil.py
@@ -0,0 +1,7 @@
+from modul1 import *
+
+lingkaran(5)
+
+from modul2 import *
+pangkat(2,3)
+akar_kuadrat(4)
